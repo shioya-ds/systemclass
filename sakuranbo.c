f838a6efd061c23a6d38d69fa2d64ca2ac9a0b12
shioya-ds <t236052@st.yamagata-u.ac.jp>
2024-10-11 19:12:34 +0900

    Add files via upload

---
+++ sakuranbo.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("この地球上の誰よりも//");
+    
+    int sakuranbo;
+    sakuranbo = 4869;
+    
+    printf("sakuranbo is: %d", sakuranbo);
+   
+    return 0;
+}
