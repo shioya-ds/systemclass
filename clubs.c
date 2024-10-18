b12f4eb71e57d8590508a01f163ba636b8293c26
shioya-ds <t236052@st.yamagata-u.ac.jp>
2024-10-18 19:28:46 +0900

    Add files via upload

---
+++ clubs.c
@@ -0,0 +1,25 @@
+int main()
+{
+    int k;
+    printf("How many students?");
+    scanf("%d", &k);
+
+    printf("The number of students is: %d", k);
+    
+    return 0;
+
+}int main()
+{
+    int k;
+    printf("How many athletic clubs in Yonezawa campus?");
+    scanf("%d", &k);
+    
+    int m;
+    printf("How many humanities clubs in Yonezawa campus?");
+    scanf("%d", &m);
+    
+
+    printf("The number of clubs in Yonezawa campus is: %d", k+m);
+    
+    return 0;
+}
